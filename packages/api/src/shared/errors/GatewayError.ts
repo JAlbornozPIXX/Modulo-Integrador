de77ab99855be45e82cb7201c56a300771122aa7
rodyherrera <contact@rodyherrera.com>
2026-09-02 11:37:08 -0400

    refactor: remove unused gateway and queue related code

--- packages/api/src/shared/errors/GatewayError.ts
+++
@@ -1,4 +0,0 @@
-import { GatewayErrors } from '@app/contracts/shared/errors';
-import { defineErrors } from '@app/core/defineErrors';
-
-export const GatewayError = defineErrors(GatewayErrors);
--- packages/api/src/shared/redis/RedisClient.ts
+++
@@ -1,8 +0,0 @@
-import { Redis } from 'ioredis';
-import { config } from '@/shared/config';
-
-export const redis = new Redis({
-    host: config.redis.host,
-    port: config.redis.port,
-    lazyConnect: true
-});
